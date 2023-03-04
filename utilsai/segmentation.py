f5f82042afb98a6cd5f300938485adb3aff13f78
eson <474420502@qq.com>
2023-03-05 01:10:34 +0800

    test success

--- utilsai/segmentation.py
+++ utilsai/segmentation.py
@@ -1,5 +1,4 @@
  
-
 special_char = ".．"
 seg_char = "。!;！；\r\t\n"
 
@@ -20,9 +19,8 @@ class Segmentation:
 
 class TextSegmentation:
  
-    def __init__(self, text) -> None:
+    def __init__(self, text, seg_char=seg_char) -> None:
         self.text = text
-        self.special_char = special_char
         self.seg_char = seg_char
         self.segmentation = []
 
@@ -59,6 +57,7 @@ class TextSegmentation:
         return self.segmentation
  
 if __name__ == "__main__":
+ 
     text = "随着社会经济的发展，糖尿病患病率逐渐增加，已成为严重的世界性问题。糖尿病特别是其慢性并发症影响患者的生活质量，甚至威胁患者的生命，给社会、家庭以及患者带来沉重的经济负担。中国的流行病学调查显示，中国20岁以上人群2型糖尿病患病率达9.7%；且近2/3的患者HbA1c得不到有效控制(HbA1c≤7%)；60.7%的患者因未被诊断而无法及早进行有效的治疗和指导。"
     ts = TextSegmentation(text)
  
