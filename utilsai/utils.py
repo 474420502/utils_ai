9fd69fde9507f534d7e3f2ffadf01833aad56d49
eson <474420502@qq.com>
2023-03-09 06:48:17 +0800

    添加 最大公约数

--- utilsai/utils.py
+++ utilsai/utils.py
@@ -15,3 +15,21 @@ def get_approximation(h):
                 break
 
     return a_h
+
+def get_max_approximation(h,w):
+    """
+        获取所有最大约数. 总乘积就是最大
+    """
+    hl = get_approximation(h)
+    wl = get_approximation(w)
+
+    hl_del_list = []
+    for i in range(len(hl)):
+        a = hl[i]
+        for b in iter(wl):
+            if a == b:
+                hl_del_list.append(a)
+                wl.remove(b)
+                break
+
+    return hl_del_list
